e46f0f00a7821600aaaf5390b2a0faf2aea60996
Gene <liujd@lamda.nju.edu.cn>
2023-04-06 22:59:32 +0800

    [MNT] Modify model API

--- learnware/learnware/base.py
+++ learnware/learnware/base.py
@@ -8,23 +8,19 @@ from ..utils import get_module_by_module_path
 
 
 class Learnware:
-    def __init__(self, id: str, name: str, model: Union[BaseModel, dict], specification: Specification):
+    def __init__(self, id: str, name: str, model: Union[BaseModel, str], specification: Specification):
         self.id = id
         self.name = name
         self.model = self._import_model(model)
         self.specification = specification
 
-    def _import_model(self, model: Union[BaseModel, dict, str]) -> BaseModel:
+    def _import_model(self, model: Union[BaseModel, str]) -> BaseModel:
         """_summary_
 
         Parameters
         ----------
-        model : Union[BaseModel, dict]
-            - If isinstance(model, dict), autoimport the model w.r.t the following format:
-                model = {
-                    "module_path": str,
-                    "class_name": str
-                }
+        model : Union[BaseModel, str]
+            - If isinstance(model, str), model is the path of the .py file
             - If isinstance(model, BaseModel), return model directly
         Returns
         -------
@@ -38,11 +34,13 @@ class Learnware:
         """
         if isinstance(model, BaseModel):
             return model
-        elif isinstance(model, dict):
-            model_module = get_module_by_module_path(model["module_path"])
-            return getattr(model_module, model["class_name"])()
         elif isinstance(model, str):
-            return model    # For test purpose
+            model_dict = {
+                "module_path": model,
+                "class_name": "Model"
+            }
+            model_module = get_module_by_module_path(model_dict["module_path"])
+            return getattr(model_module, model_dict["class_name"])()
         else:
             raise TypeError("model must be BaseModel or dict")
 
