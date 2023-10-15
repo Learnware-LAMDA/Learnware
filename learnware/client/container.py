35d480385fd445fb12174a38978bb268a3c88db3
bxdd <bxddream@gmail.com>
2023-10-16 01:39:41 +0800

    [MNT] black format

--- learnware/client/container.py
+++ learnware/client/container.py
@@ -16,29 +16,29 @@ from ..logger import get_module_logger
 
 logger = get_module_logger(module_name="client_container")
 
+
 class ModelContainer(BaseModel):
-    def __init__(self, model_config: dict, learnware_zippath: str, build: bool=True):
+    def __init__(self, model_config: dict, learnware_zippath: str, build: bool = True):
         self.model_script = os.path.join(C.package_path, "client", "scripts", "run_model.py")
         self.model_config = model_config
         self.learnware_zippath = learnware_zippath
         self.build = build
         self.cleanup_flag = False
-    
+
     def reset(self, **kwargs):
         for _k, _v in kwargs.items():
             if hasattr(self, _k):
                 setattr(_k, _v)
-                
+
     def init_env_and_set_metadata(self):
-        """We must set `input_shape` and `output_shape` 
-        """
+        """We must set `input_shape` and `output_shape`"""
         if self.build:
             self.cleanup_flag = True
             self._init_env()
             atexit.register(self.remove_env)
-        
+
         self._set_metadata()
-        
+
     def remove_env(self):
         if self.cleanup_flag is True:
             self.cleanup_flag = False
@@ -47,37 +47,37 @@ class ModelContainer(BaseModel):
             except Exception as err:
                 self.cleanup_flag = True
                 raise err
-    
+
     def _set_metadata(self):
-        raise NotImplementedError('_set_metadata method is not implemented!')
-        
+        raise NotImplementedError("_set_metadata method is not implemented!")
+
     def _init_env(self):
-        raise NotImplementedError('_init_env method is not implemented!')
-        
+        raise NotImplementedError("_init_env method is not implemented!")
+
     def _remove_env(self):
-        raise NotImplementedError('_remove_env method is not implemented!')
-    
+        raise NotImplementedError("_remove_env method is not implemented!")
+
     def fit(self, X, y):
-        raise NotImplementedError('fit method is not implemented!')
+        raise NotImplementedError("fit method is not implemented!")
 
     def predict(self, X):
-        raise NotImplementedError('predict method is not implemented!')
+        raise NotImplementedError("predict method is not implemented!")
 
     def finetune(self, X, y) -> None:
-        raise NotImplementedError('finetune method is not implemented!')
-        
+        raise NotImplementedError("finetune method is not implemented!")
+
+
 class ModelCondaContainer(ModelContainer):
-    
-    def __init__(self, model_config: dict, learnware_zippath: str, conda_env: str=None, build: bool=True):
+    def __init__(self, model_config: dict, learnware_zippath: str, conda_env: str = None, build: bool = True):
         self.conda_env = f"learnware_{shortuuid.uuid()}" if conda_env is None else conda_env
         super(ModelCondaContainer, self).__init__(model_config, learnware_zippath, build)
-        
+
     def _init_env(self):
         install_environment(self.learnware_zippath, self.conda_env)
-    
+
     def _remove_env(self):
         remove_enviroment(self.conda_env)
-        
+
     def _set_metadata(self):
         with tempfile.TemporaryDirectory(prefix="learnware_") as tempdir:
             output_path = os.path.join(tempdir, "output.pkl")
@@ -158,8 +158,11 @@ class ModelCondaContainer(ModelContainer):
     def finetune(self, X, y) -> None:
         self._run_model_with_script("finetune", X=X, y=y)
 
+
 class ModelDockerContainer(ModelCondaContainer):
-    def __init__(self, model_config: dict, learnware_zippath: str, docker_img=None, conda_env: str=None, build: bool=True):
+    def __init__(
+        self, model_config: dict, learnware_zippath: str, docker_img=None, conda_env: str = None, build: bool = True
+    ):
         """_summary_
 
         Parameters
@@ -168,32 +171,39 @@ class ModelDockerContainer(ModelCondaContainer):
             Whether to build the docker env, by default True
         """
         if docker_img is None:
-            self.docker_img = None # create docker img
+            self.docker_img = None  # create docker img
         self.conda_env = f"learnware_{shortuuid.uuid()}" if conda_env is None else conda_env
         # call init method of parent of parent class
-        super(ModelCondaContainer, self).__init__(model_config, learnware_zippath, True if docker_img is None else build)
-        
+        super(ModelCondaContainer, self).__init__(
+            model_config, learnware_zippath, True if docker_img is None else build
+        )
+
     def _set_metadata(self):
-        raise NotImplementedError('_set_metadata method is not implemented!')
-        
+        raise NotImplementedError("_set_metadata method is not implemented!")
+
     def _init_env(self):
-        raise NotImplementedError('_init_env method is not implemented!')
-        
+        raise NotImplementedError("_init_env method is not implemented!")
+
     def _remove_env(self):
-        raise NotImplementedError('_remove_env method is not implemented!')
-    
+        raise NotImplementedError("_remove_env method is not implemented!")
+
     def fit(self, X, y):
-        raise NotImplementedError('fit method is not implemented!')
+        raise NotImplementedError("fit method is not implemented!")
 
     def predict(self, X):
-        raise NotImplementedError('predict method is not implemented!')
+        raise NotImplementedError("predict method is not implemented!")
 
     def finetune(self, X, y) -> None:
-        raise NotImplementedError('finetune method is not implemented!')
-        
+        raise NotImplementedError("finetune method is not implemented!")
+
+
 class LearnwaresContainer:
     def __init__(
-        self, learnwares: Union[List[Learnware], Learnware], learnware_zippaths: Union[List[str], str], cleanup=True, mode='conda'
+        self,
+        learnwares: Union[List[Learnware], Learnware],
+        learnware_zippaths: Union[List[str], str],
+        cleanup=True,
+        mode="conda",
     ):
         """The initializaiton method for base reuser
 
@@ -210,40 +220,44 @@ class LearnwaresContainer:
         assert all(
             [isinstance(_learnware.get_model(), dict) for _learnware in learnwares]
         ), "the learnwre model should not be instantiated for reuser with containter"
-        
-        if mode == 'conda':
+
+        if mode == "conda":
             self.learnware_list = [
                 Learnware(
                     _learnware.id, ModelCondaContainer(_learnware.get_model(), _zippath), _learnware.get_specification()
                 )
                 for _learnware, _zippath in zip(learnwares, learnware_zippaths)
             ]
-        elif mode == 'docker':
+        elif mode == "docker":
             docker_img = self._generate_docker_img()
             self.learnware_list = [
                 Learnware(
-                    _learnware.id, ModelDockerContainer(_learnware.get_model(), _zippath, docker_img), _learnware.get_specification()
+                    _learnware.id,
+                    ModelDockerContainer(_learnware.get_model(), _zippath, docker_img),
+                    _learnware.get_specification(),
                 )
                 for _learnware, _zippath in zip(learnwares, learnware_zippaths)
             ]
-        
+
         else:
             raise ValueError(f"mode must be in ['conda', 'docker'], should not be {mode}")
         self.results = [True] * len(learnwares)
         self.cleanup = cleanup
-        print('234', self.learnware_list)
+        print("234", self.learnware_list)
 
     def _generate_docker_img():
         return None
-        
+
     def __enter__(self):
         model_list = [_learnware.get_model() for _learnware in self.learnware_list]
         with ProcessPoolExecutor(max_workers=max(os.cpu_count() // 2, 1)) as executor:
             results = executor.map(self._initialize_model_container, model_list)
         self.results = list(results)
-        print('234', self.results, sum(self.results), len(self.learnware_list))
+        print("234", self.results, sum(self.results), len(self.learnware_list))
         if sum(self.results) < len(self.learnware_list):
-            logger.warning(f'{len(self.learnware_list) - sum(results)} of {len(self.learnware_list)} learnwares init failed! This learnware will be ignored')
+            logger.warning(
+                f"{len(self.learnware_list) - sum(results)} of {len(self.learnware_list)} learnwares init failed! This learnware will be ignored"
+            )
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -262,7 +276,7 @@ class LearnwaresContainer:
             logger.error(f"build env {model.conda_env} failed due to {err}")
             return False
         return True
-    
+
     @staticmethod
     def _destroy_model_container(model: ModelCondaContainer):
         try:
@@ -274,5 +288,5 @@ class LearnwaresContainer:
 
     def get_learnwares_with_container(self):
         learnwares = [_learnware for _learnware, _result in zip(self.learnware_list, self.results) if _result is True]
-        print('233', learnwares, list(self.results))
+        print("233", learnwares, list(self.results))
         return learnwares
