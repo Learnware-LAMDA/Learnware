bef000fb0a14d3f3ea1ddd18043c22835e469c03
bxdd <bxddream@gmail.com>
2023-10-27 03:20:00 +0800

    [MNT] black format

--- learnware/market/module.py
+++ learnware/market/module.py
@@ -2,18 +2,18 @@ from .base import LearnwareMarket
 from .easy2 import EasyChecker, EasyOrganizer, EasySearcher
 
 MARKET_CONFIG = {
-    'easy': {
-        'organizer': EasyOrganizer(),
-        'checker': EasyChecker(),
-        'searcher': EasySearcher(),
+    "easy": {
+        "organizer": EasyOrganizer(),
+        "checker": EasyChecker(),
+        "searcher": EasySearcher(),
     }
 }
 
-def instatiate_learnware_market(market_id, name='easy'):
-    
+
+def instatiate_learnware_market(market_id, name="easy"):
     return LearnwareMarket(
         market_id=market_id,
-        organizer=MARKET_CONFIG[name]['organizer'],
-        checker=MARKET_CONFIG[name]['checker'],
-        searcher=MARKET_CONFIG[name]['searcher'],
+        organizer=MARKET_CONFIG[name]["organizer"],
+        checker=MARKET_CONFIG[name]["checker"],
+        searcher=MARKET_CONFIG[name]["searcher"],
     )
