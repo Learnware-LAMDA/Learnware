e8d1550a74f14b9dbea78849cf80477577173230
Gene <liujd@lamda.nju.edu.cn>
2023-10-30 20:38:12 +0800

    [ENH] add test for image rkme

--- tests/test_specification/test_rkme.py
+++ tests/test_specification/test_rkme.py
@@ -1,18 +1,19 @@
 import os
 import json
+import torch
 import unittest
 import tempfile
 import numpy as np
 
 import learnware
-import learnware.specification as specification
-from learnware.specification import RKMEStatSpecification
+from learnware.specification import RKMEStatSpecification, RKMEImageStatSpecification
 
 
 class TestRKME(unittest.TestCase):
     def test_rkme(self):
         X = np.random.uniform(-10000, 10000, size=(5000, 200))
-        rkme = specification.utils.generate_rkme_spec(X)
+        rkme = RKMEStatSpecification()
+        rkme.generate_stat_spec_from_data(X)
 
         with tempfile.TemporaryDirectory(prefix="learnware_") as tempdir:
             rkme_path = os.path.join(tempdir, "rkme.json")
@@ -25,6 +26,31 @@ class TestRKME(unittest.TestCase):
             rkme2 = RKMEStatSpecification()
             rkme2.load(rkme_path)
             assert rkme2.type == "RKMEStatSpecification"
+
+    def test_image_rkme(self):
+        def _test_image_rkme(X):
+            image_rkme = RKMEImageStatSpecification()
+            image_rkme.generate_stat_spec_from_data(X, resize=True)
+
+            with tempfile.TemporaryDirectory(prefix="learnware_") as tempdir:
+                rkme_path = os.path.join(tempdir, "rkme.json")
+                rkme.save(rkme_path)
+
+                with open(rkme_path, "r") as f:
+                    data = json.load(f)
+                    assert data["type"] == "RKMEImageStatSpecification"
+
+                rkme2 = RKMEImageStatSpecification()
+                rkme2.load(rkme_path)
+                assert rkme2.type == "RKMEImageStatSpecification"
+
+        _test_image_rkme(np.random.randint(0, 255, size=(2000, 3, 32, 32)))
+        _test_image_rkme(np.random.randint(0, 255, size=(2000, 3, 128, 128)))
+        _test_image_rkme(np.random.randint(0, 255, size=(2000, 3, 128, 128)) / 255)
+
+        _test_image_rkme(torch.randint(0, 255, (2000, 3, 32, 32)))
+        _test_image_rkme(torch.randint(0, 255, (2000, 3, 128, 128)))
+        _test_image_rkme(torch.randint(0, 255, (2000, 3, 128, 128)) / 255)
 
 
 if __name__ == "__main__":
