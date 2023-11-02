a53e618fdeaf3de92460f94ff3b81e05d2e36c93
bxdd <bxddream@gmail.com>
2023-11-03 03:40:23 +0800

    [MNT] fix reuser with many specifications

--- learnware/market/utils.py
+++ learnware/market/utils.py
@@ -1,12 +1,7 @@
 from ..specification import Specification
-from ..logger import get_module_logger
 
-logger = get_module_logger("market_utils")
-
-
-def parse_specification_type(stat_spec: Specification):
+def parse_specification_type(stat_spec: Specification, spec_list = ["RKMETableSpecification", "RKMETextSpecification", "RKMEImageSpecification"]):
     stat_specs = stat_spec.stat_spec
-    spec_list = ["RKMETableSpecification", "RKMETextSpecification", "RKMEImageSpecification"]
     for spec in spec_list:
         if spec in stat_specs:
             return spec
