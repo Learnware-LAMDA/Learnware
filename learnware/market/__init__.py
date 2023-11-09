834bdd481b7a7804309dcfa76a03ecf600307b42
Gene <liujd@lamda.nju.edu.cn>
2023-11-09 10:27:34 +0800

    [MNT] rm extra DatabaseOperations

--- learnware/market/__init__.py
+++ learnware/market/__init__.py
@@ -5,6 +5,5 @@ from .evolve import EvolvedOrganizer
 from .easy2 import EasyOrganizer, EasySearcher, EasySemanticChecker, EasyStatChecker
 from .hetergeneous import HeterogeneousOrganizer, MappingFunction
 
-from .easy import EasyMarket
 from .classes import CondaChecker
 from .module import instantiate_learnware_market
