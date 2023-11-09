baf86091cda30f98e25cc4e11a93a32f4726bc4d
bxdd <bxddream@gmail.com>
2023-11-09 14:42:43 +0800

    [MNT] update support python verison

--- setup.py
+++ setup.py
@@ -108,8 +108,10 @@ if __name__ == "__main__":
             "Operating System :: POSIX :: Linux",
             "Operating System :: Microsoft :: Windows",
             "Operating System :: MacOS",
-            "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
         ],
     )
