35d1b7123835ac3d3297c1fac6c630f746da8741
bxdd <bxddream@gmail.com>
2023-11-09 13:37:20 +0800

    [MNT] rename easy2 to easy

--- learnware/market/__init__.py
+++ learnware/market/__init__.py
@@ -2,7 +2,7 @@ from .anchor import AnchoredUserInfo, AnchoredOrganizer
 from .base import BaseUserInfo, LearnwareMarket, BaseChecker, BaseOrganizer, BaseSearcher
 from .evolve_anchor import EvolvedAnchoredOrganizer
 from .evolve import EvolvedOrganizer
-from .easy2 import EasyOrganizer, EasySearcher, EasySemanticChecker, EasyStatChecker
+from .easy import EasyOrganizer, EasySearcher, EasySemanticChecker, EasyStatChecker
 from .hetergeneous import HeterogeneousOrganizer, MappingFunction
 
 from .classes import CondaChecker
