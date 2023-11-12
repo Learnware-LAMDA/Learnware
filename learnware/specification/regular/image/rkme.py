9be1495a2f68fe04d796bb31a9d13432ea4f37fb
shihy <xingyuncao2002@outlook.com>
2023-11-12 16:29:38 +0800

    [MNT] modify error msg

--- learnware/specification/regular/image/rkme.py
+++ learnware/specification/regular/image/rkme.py
@@ -98,9 +98,7 @@ class RKMEImageSpecification(RegularStatsSpecification):
 
         """
         if len(X.shape) != 4:
-            raise ValueError(
-                "X should be in shape of [N, C, {0:d}, {0:d}]. ".format(RKMEImageSpecification.IMAGE_WIDTH)
-            )
+            raise ValueError("X should be in shape of [N, C, H, W]. ")
 
         if (
             X.shape[2] != RKMEImageSpecification.IMAGE_WIDTH or X.shape[3] != RKMEImageSpecification.IMAGE_WIDTH
