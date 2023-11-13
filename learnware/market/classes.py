fdfcee002ef6fbb9873e5aed23f9b79007a31b37
Gene <liujd@lamda.nju.edu.cn>
2023-11-13 11:25:03 +0800

    [FIX] fix bugs in checker return

--- learnware/market/classes.py
+++ learnware/market/classes.py
@@ -16,11 +16,11 @@ class CondaChecker(BaseChecker):
         try:
             with LearnwaresContainer(learnware, ignore_error=False) as env_container:
                 learnwares = env_container.get_learnwares_with_container()
-                check_status = self.inner_checker(learnwares[0])
+                check_status, message = self.inner_checker(learnwares[0])
         except Exception as e:
             traceback.print_exc()
             message = f"Conda Checker failed due to installed learnware failed and {e}"
             logger.warning(message)
             message += "\n" + traceback.format_exc()
             return BaseChecker.INVALID_LEARNWARE, message
-        return check_status
+        return check_status, message
