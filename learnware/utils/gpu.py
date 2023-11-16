124448a86dec7b53345327dcd6f8d318f8094c68
bxdd <bxddream@gmail.com>
2023-11-16 23:32:25 +0800

    [MNT] update cuda manager

--- learnware/utils/gpu.py
+++ learnware/utils/gpu.py
@@ -50,13 +50,34 @@ def choose_device(cuda_idx=-1):
 class CudaManager:
     def __init__(self):
         if is_torch_available(verbose=False):
-            pass
+            import torch
 
-    def get_specification_cuda_idx(self):
-        pass
+            self.cuda_avalable = torch.cuda.is_available()
+            self.cuda_count = torch.cuda.device_count() if self.cuda_avalable else 0
+        else:
+            self.cuda_avalable = False
+            self.cuda_count = 0
 
-    def get_cpu_idx(self):
-        pass
+        self.cur_cuda_idx = 0
+        self.stat_spec_cuda = {}
 
-    def get_random_cuda_idx(self):
-        pass
+    def reset(self):
+        self.cur_cuda_idx = 0
+        self.stat_spec_cuda = {}
+
+    def allocate_cuda(self):
+        if not self.cuda_avalable:
+            return -1
+
+        ret_cuda_idx = self.cur_cuda_idx
+        self.cur_cuda_idx = (self.cur_cuda_idx + 1) % self.cuda_count
+        return ret_cuda_idx
+
+    def allocate_stat_spec_cuda(self, stat_spec):
+        if stat_spec.type not in self.stat_spec_cuda:
+            self.stat_spec_cuda[stat_spec.type] = self.allocate_cuda()
+
+        return self.stat_spec_cuda[stat_spec.type]
+
+
+cuda_manager = CudaManager()
