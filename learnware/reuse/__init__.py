9f6c779db608d416c5fb534a27113b2977f5022b
bxdd <bxddream@gmail.com>
2023-11-24 00:41:04 +0800

    [FIX] fix bug

--- learnware/reuse/__init__.py
+++ learnware/reuse/__init__.py
@@ -2,7 +2,7 @@ from .base import BaseReuser
 from .align import AlignLearnware
 
 from ..logger import get_module_logger
-from ..utils import is_torch_available, get_platform, SystemType
+from ..utils import is_torch_available
 
 logger = get_module_logger("reuse")
 
@@ -12,11 +12,13 @@ if not is_torch_available(verbose=False):
     HeteroMapAlignLearnware = None
     FeatureAlignLearnware = None
     JobSelectorReuser = None
+    EnsemblePruningReuser = None
     logger.error(
-        "[AveragingReuser, FeatureAugmentReuser, HeteroMapAlignLearnware, FeatureAlignLearnware, JobSelectorReuser] are skipped due to 'torch' is not installed!"
+        "[AveragingReuser, FeatureAugmentReuser, HeteroMapAlignLearnware, FeatureAlignLearnware, JobSelectorReuser, EnsemblePruningReuser] are skipped due to 'torch' is not installed!"
     )
 else:
     from .averaging import AveragingReuser
     from .feature_augment import FeatureAugmentReuser
     from .hetero import HeteroMapAlignLearnware, FeatureAlignLearnware
     from .job_selector import JobSelectorReuser
+    from .ensemble_pruning import EnsemblePruningReuser
