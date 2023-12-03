235495cfc2b0b939e6d49b24221add143fa2ab24
GeneLiuXe <356340460@qq.com>
2023-12-03 14:05:32 +0800

    [ENH] add install env script for docker

---
+++ learnware/client/scripts/install_env.py
@@ -0,0 +1,15 @@
+import argparse
+from learnware.client.utils import install_environment
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--learnware-dirpath", type=str, required=True, help="path of learnware dir")
+    parser.add_argument("--conda-env", type=str, required=False, help="name of conda env")
+
+    args = parser.parse_args()
+
+    learnware_dirpath = args.learnware_dirpath
+    conda_env = args.conda_env
+
+    install_environment(learnware_dirpath, conda_env)
