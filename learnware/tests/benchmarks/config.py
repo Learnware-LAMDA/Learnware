cf4ad803b2b502381df2cd45d08ea637d49ec027
bxdd <bxddream@gmail.com>
2023-12-23 22:02:47 +0800

    [MNT] update tests BenchmarkConfig

--- learnware/tests/benchmarks/config.py
+++ learnware/tests/benchmarks/config.py
@@ -12,13 +12,4 @@ class BenchmarkConfig:
     extra_info_path: Optional[str] = None
 
 
-benchmark_configs = {
-    "example": BenchmarkConfig(
-        name="example",
-        user_num=3,
-        learnware_ids=["00001951", "00001980", "00001987"],
-        test_data_path="example_path1",
-        train_data_path="example_path2",
-        extra_info_path="example_path3",
-    )
-}
+benchmark_configs = {}
