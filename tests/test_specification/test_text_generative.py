0e6b96c3522b1712eabd6dba9644b0ca4eaca557
zzc <1196529906@qq.com>
2025-01-09 16:53:26 +0800

    [MNT] modify test_text_generative.py

--- tests/test_specification/test_text_generative.py
+++ tests/test_specification/test_text_generative.py
@@ -13,16 +13,7 @@ from learnware.market.llm import LLMStatSearcher
 from learnware.specification.base import Specification
 from learnware.specification.regular.text import GenerativeModelSpecification
 
-
-# Import from our project
-
-if os.path.expanduser(os.environ["LIB_PATH"]) not in sys.path:
-    sys.path.append(os.path.expanduser(os.environ["LIB_PATH"]))
-
-
-from src.datasets.llm.utils import set_seed, prepare_train_data
-from src.datasets.llm.benchmark import Benchmark
-
+from text_generative_utils import DATASET, prepare_data
 
 class TestGenerativeModelSpecification(unittest.TestCase):
     @staticmethod
@@ -67,18 +58,15 @@ class TestGenerativeModelSpecification(unittest.TestCase):
             assert spec2.type == "GenerativeModelSpecification"
 
     def test_generating_spec(self):
-        benchmark = Benchmark("medical")
-        train_dataset = benchmark.get_user_dataset("pubmedqa")
+        train_dataset = prepare_data(DATASET["pubmedqa"])
         
         self._test_with_X(train_dataset["text"])
         self._test_with_dataset(train_dataset)
         
     def test_searching_spec(self):
-        benchmark = Benchmark("medical")
-        
         specs, learnwares = [], []
         for i, dataset_name in enumerate(["pubmedqa", "medmcqa"]):
-            train_dataset = benchmark.get_user_dataset("pubmedqa")
+            train_dataset = prepare_data(DATASET[dataset_name])
         
             spec = GenerativeModelSpecification(max_steps=5)
             spec.generate_stat_spec_from_data(dataset=train_dataset)
