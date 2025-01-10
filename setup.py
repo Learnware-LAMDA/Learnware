63a7079ef65fb1d69fd9c4277edf60d78f2d1429
HaoyuShi <shi_haoyu@outlook.com>
2025-01-10 12:09:11 +0800

    [MNT] Adding Dependencies

--- setup.py
+++ setup.py
@@ -53,6 +53,10 @@ REQUIRED = [
     "portalocker>=2.0.0",
     "qpsolvers[clarabel]>=4.0.1",
     "geatpy>=2.7.0;python_version<'3.11'",
+    "trl>=0.13.0",
+    "datasets>=3.2.0",
+    "peft>=0.14.0",
+    "lm_eval>=0.4.7"
 ]
 
 DEV_REQUIRED = [
