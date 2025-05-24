be4d7d38e1814b0e14e328bebcebdb6c8191e414
Asymptotez <201220101@smail.nju.edu.cn>
2025-05-24 23:05:36 +0800

    [FIX] update datasets version to 2.16.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ REQUIRED = [
     "qpsolvers[clarabel]>=4.0.1",
     "geatpy>=2.7.0;python_version<'3.11'",
     "trl>=0.11.4",
-    "datasets>=3.1.0",
+    "datasets>=2.16.0",
     "peft>=0.13.2",
     "lm_eval>=0.4.7"
 ]
